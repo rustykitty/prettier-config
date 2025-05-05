64829854d151ba78ffe294adcaebc3f399f8de34
Rusty <rusty3925@gmail.com>
2025-05-05 07:53:05 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import bristlefrostPrettierConfig from "@bristlefrost/prettier-config";
+
+const config = {
+    ...bristlefrostPrettierConfig,
+    useTabs: false,
+    printWidth: 80,
+};
+
+export default config;
