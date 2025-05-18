76de53dc3bfa29bfa5c06b6880221c4784cb837d
Rusty <rusty3925@gmail.com>
2025-05-18 15:04:32 -0700

    remove printWidth of 80 as too short- bristlefrost's config has 120

--- index.mjs
+++ index.mjs
@@ -3,7 +3,6 @@ import bristlefrostPrettierConfig from "@bristlefrost/prettier-config";
 const config = {
     ...bristlefrostPrettierConfig,
     useTabs: false,
-    printWidth: 80,
 };
 
 export default config;
